828d3d09662118bbfc990c37ea494788d83e17ee
Faruk <farukt9@gmail.com>
2024-08-21 23:57:23 +0200

    normaliseURL + tests

---
+++ crawl.test.js
@@ -0,0 +1,30 @@
+const { normaliseURL } = require('./crawl.js')
+const { test, expect } = require('@jest/globals')
+
+test('normaliseURL strip protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normaliseURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normaliseURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normaliseURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normaliseURL capitals', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normaliseURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normaliseURL strip http', () => {
+    const input = 'http://blog.boot.dev/path/'
+    const actual = normaliseURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
