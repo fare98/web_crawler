f30d63448af6f4f039d3a980bb1957625bd918f5
Faruk <farukt9@gmail.com>
2024-08-25 21:24:04 +0200

    printReport

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //console.log('hello world')
 
 const { crawlPage } = require('./crawl.js')
+const { printReport } = require('./report.js')
 
 
 
@@ -20,10 +21,11 @@ async function main ()  {
 
     console.log(`starting crawl of ${baseURL}`)
     const pages = await crawlPage(baseURL, baseURL, {})
+    printReport(pages)
 
-    for (const page of Object.entries(pages)){
+    /*for (const page of Object.entries(pages)){
         console.log(page)
-    }
+    }*/
 
 
 }
---
+++ report.test.js
@@ -0,0 +1,38 @@
+const { sortPages } = require('./report.js')
+const { test, expect } = require('@jest/globals')
+
+
+
+test('sortPages 2 pages', () => {
+    const input = {
+        'http://wagslane.dev/path': 1,
+        'http://wagslane.dev': 3
+    }
+    const actual = sortPages(input)
+    const expected = [
+        ['http://wagslane.dev', 3],
+        ['http://wagslane.dev/path', 1]
+    ]
+    expect(actual).toEqual(expected)
+})
+
+
+
+test('sortPages 5 pages', () => {
+    const input = {
+        'http://wagslane.dev/path': 1,
+        'http://wagslane.dev': 3,
+        'http://wagslane.dev/path2': 5,
+        'http://wagslane.dev/path3': 9,
+        'http://wagslane.dev/path4': 7
+    }
+    const actual = sortPages(input)
+    const expected = [
+        ['http://wagslane.dev/path3', 9],
+        ['http://wagslane.dev/path4', 7],
+        ['http://wagslane.dev/path2', 5],
+        ['http://wagslane.dev', 3],
+        ['http://wagslane.dev/path', 1]
+    ]
+    expect(actual).toEqual(expected)
+})
